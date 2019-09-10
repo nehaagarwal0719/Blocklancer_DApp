ecde81c44b9cc7e215160473ac62a7903b6dabbb
nehaagarwal0719 <nehaagarwal0464@gmail.com>
2019-09-10 20:51:02 +0530

    third

---
+++ client/src/components/Profile.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import jwt_decode from 'jwt-decode'
+
+class Profile extends Component {
+  constructor() {
+    super()
+    this.state = {
+      fname: '',
+      lname: '',
+      email: '',
+      errors: {}
+    }
+  }
+
+  componentDidMount() {
+    const token = localStorage.usertoken
+    const decoded = jwt_decode(token)
+    this.setState({
+      fname: decoded.fname,
+      lname: decoded.lname,
+      email: decoded.email
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron mt-5">
+          <div className="col-sm-8 mx-auto">
+            <h1 className="text-center">PROFILE</h1>
+          </div>
+          <table className="table col-md-6 mx-auto">
+            <tbody>
+              <tr>
+                <td>Fist Name</td>
+                <td>{this.state.fname}</td>
+              </tr>
+              <tr>
+                <td>Last Name</td>
+                <td>{this.state.lname}</td>
+              </tr>
+              <tr>
+                <td>Email</td>
+                <td>{this.state.email}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Profile
