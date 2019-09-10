38d1d87a614e231ede5506d9c51f8b580654bba6
nehaagarwal0719 <nehaagarwal0464@gmail.com>
2019-09-10 20:45:58 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize')
+const db = require('../database/db.js')
+
+module.exports = db.sequelize.define(
+  'users',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    fname: {
+      type: Sequelize.STRING
+    },
+    lname: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    created: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    }
+  },
+  {
+    timestamps: false
+  }
+)
